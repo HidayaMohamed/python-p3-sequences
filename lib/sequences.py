2eeab46a56f57a54440db2944aab19e53d949e42
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-13 11:05:51 +0300

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+   if length <= 0:
+      print([])
+      return
+   elif length ==1 :
+      print ([0])
+      return
+   fib_sequence = [0, 1]
+   while len(fib_sequence) < length :
+      new_num = fib_sequence[-1] + fib_sequence[-2]
+      fib_sequence.append(new_num)
+
+   print (fib_sequence)
+   
